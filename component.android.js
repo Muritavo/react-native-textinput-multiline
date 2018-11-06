90c70a6b8a3d066ce15c1317a167828ef8ad2f9f
Murilo Oliveira de Araujo <muritavo@outlook.com>
2018-11-06 11:36:53 -0200

    Renomeado o componente Android

---
+++ component.android.js
@@ -0,0 +1,5 @@
+import { requireNativeComponent } from "react-native";
+
+const TextInput = requireNativeComponent('RNReactNativeTextinputMultiline');
+
+export default TextInput;
--- index.android.js
+++
@@ -1,5 +0,0 @@
-import { NativeModules } from 'react-native';
-
-const { RNReactNativeTextinputMultiline } = NativeModules;
-
-export default RNReactNativeTextinputMultiline;
